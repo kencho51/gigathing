cf031af0678f936848ddbc01907e368abb51b45c
Ken <kencho.gigascience@gmail.com>
2020-06-29 10:26:44 +0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,22 +5,22 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-printf "Build the project.\n"
+printf "!!!Build the project.\n"
 #hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 hugo -t ghostwriter
 
-printf "Go To Public folder\n"
+printf "!!!Go To Public folder\n"
 cd public
 
-printf "Add changes to git.\n"
+printf "!!!Add changes to git.\n"
 git add .
 
-printf "Commit changes.\n"
+printf "!!!Commit changes.\n"
 msg="Rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
 
-printf "Push source and build repos.\n"
+printf "!!!Push source and build repos.\n"
 git push origin master
